249946c7efff2e801becc6d2d6d86db08dea8982
吴浩峥 <wuhaozheng@wuhaozhengdeMacBook-Pro.local>
2018-01-30 21:30:35 -0500

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Haozheng Wu on 1/30/18.
+//  Copyright © 2018 Haozheng Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
